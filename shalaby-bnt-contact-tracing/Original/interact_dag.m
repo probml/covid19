454495b1797a54977d966f6a583244dd8826b064
Kevin Murphy <murphyk@gmail.com>
2020-06-22 15:25:47 -0700

    cleanup

--- shalaby-bnt-contact-tracing/Original/interact_dag.m
+++
@@ -1,9 +0,0 @@
-function interact_dag(contacts_t,t)
-    contact_pair=combntns(contacts_t,2);
-    for c=1:length(contact_pair)
-       i=contact_pair(1,1);
-       j=contact_pair(1,2) ;
-       dag(idx(i,t),idx(j,t+1)) = true;
-       dag(idx(j,t),idx(i,t+1)) = true;
-    end
-end
--- shalaby-bnt-contact-tracing/main.py
+++
@@ -1,7 +0,0 @@
-# Explore data from http://www.sociopatterns.org/datasets/infectious-sociopatterns/
-
-import networkx 
-import summigraph
-path='data/cumulative_2009-04-28.gml';
-#G=networkx.read_gml(path);
-G=igraph.load(path)
